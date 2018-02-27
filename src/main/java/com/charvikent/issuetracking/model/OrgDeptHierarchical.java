d706bb046cedf8bcffaa6d0364df4820ee695a8b
raju <raju@raju>
2018-02-27 12:21:31 +0530

    OrgDeptHierarchical model class and OrgChart jquery files

---
+++ src/main/java/com/charvikent/issuetracking/model/OrgDeptHierarchical.java
@@ -0,0 +1,30 @@
+package com.charvikent.issuetracking.model;
+
+public class OrgDeptHierarchical {
+
+	private String name;
+	private String id;
+	private String parent;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getParent() {
+		return parent;
+	}
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+
+
+
+
+}
